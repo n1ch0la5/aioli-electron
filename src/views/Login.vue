ad4dfd1105ac01f8cd0fa989ea0454b84a223cbe
n1ch0la5 <nicholas.johnson78@gmail.com>
2025-02-28 20:34:55 -0500

    login styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -24,46 +24,46 @@ const password = ref(import.meta.env.VITE_LARAVEL_PASS)
   ```
 -->
 <template>
-    <div class="w-full h-full bg-zinc-200">
+    <div class="w-full h-full bg-zinc-200 dark:bg-zinc-900 rounded-2xl">
     <div class="flex min-h-full flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div class="w-full max-w-sm space-y-10">
         <div>
-          <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-zinc-900">Sign in to your account</h2>
+          <h1 class="mx-auto text-center text-3xl font-bold text-indigo-600 dark:text-indigo-400">Company Name</h1>
+          <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-zinc-900 dark:text-zinc-100">Sign in to your account</h2>
         </div>
         <form class="space-y-6" action="#" method="POST">
           <div class="relative -space-y-px rounded-md shadow-sm">
-            <div class="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-zinc-300" />
+            <div class="pointer-events-none absolute inset-0 z-10 rounded-md ring-1 ring-inset ring-zinc-300 dark:ring-zinc-600" />
             <div>
               <label for="email-address" class="sr-only">Email address</label>
-              <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 text-zinc-900 ring-1 ring-inset ring-zinc-100 placeholder:text-zinc-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address" />
+              <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 text-zinc-900 dark:text-zinc-100 bg-white dark:bg-zinc-800 ring-1 ring-inset ring-zinc-100 dark:ring-zinc-700 placeholder:text-zinc-400 dark:placeholder:text-zinc-500 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Email address" />
             </div>
             <div>
               <label for="password" class="sr-only">Password</label>
-              <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-zinc-900 ring-1 ring-inset ring-zinc-100 placeholder:text-zinc-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password" />
+              <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-zinc-900 dark:text-zinc-100 bg-white dark:bg-zinc-800 ring-1 ring-inset ring-zinc-100 dark:ring-zinc-700 placeholder:text-zinc-400 dark:placeholder:text-zinc-500 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:focus:ring-indigo-500 sm:text-sm sm:leading-6" placeholder="Password" />
             </div>
           </div>
   
           <div class="flex items-center justify-between">
             <div class="flex items-center">
-              <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-zinc-300 text-indigo-600 focus:ring-indigo-600" />
-              <label for="remember-me" class="ml-3 block text-sm leading-6 text-zinc-900">Remember me</label>
+              <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-zinc-300 dark:border-zinc-600 bg-white dark:bg-zinc-800 text-indigo-600 focus:ring-indigo-600 dark:focus:ring-indigo-500" />
+              <label for="remember-me" class="ml-3 block text-sm leading-6 text-zinc-900 dark:text-zinc-300">Remember me</label>
             </div>
   
             <div class="text-sm leading-6">
-              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Forgot password?</a>
             </div>
           </div>
   
           <div>
-            <button @click.stop.prevent="login(email, password)" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+            <button @click.stop.prevent="login(email, password)" type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-700 dark:hover:bg-indigo-600 dark:focus-visible:outline-indigo-500">Sign in</button>
           </div>
         </form>
   
-        <p class="text-center text-sm leading-6 text-zinc-500">
+        <p class="text-center text-sm leading-6 text-zinc-500 dark:text-zinc-400">
           Not a member?
           {{ ' ' }}
-          <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Start a 14-day free trial</a>
+          <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500 dark:text-indigo-400 dark:hover:text-indigo-300">Start a 14-day free trial</a>
         </p>
       </div>
     </div>
